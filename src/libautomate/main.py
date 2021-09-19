89b50d96aab3ed833a5b86fa8751582ca1eeef14
Abhijit Nandy <abhijit.nandy@gmail.com>
2021-09-19 22:39:50 +0530

    Adding zeroInByImgInListSeqAndClick() for checking multiple possibilities at every level using a list of lists

--- src/libautomate/main.py
+++ src/libautomate/main.py
@@ -38,6 +38,39 @@ def zeroInByImgSeqAndClick(listImgPaths):
     return -1 # error!
 
 
+def zeroInByImgInListSeqAndClick(listImgsInListPaths):
+    idx = 1
+    screenSize = pyautogui.size()
+    # print(screenSize)
+    imgLocation = None
+    #Start with whole screen search    
+    regionRect = (0, 0, screenSize[0], screenSize[1])
+    # Go through each list in listImgsInListPaths
+    # Each entry is also a list containing possibilities for that level
+    for listImgPaths in listImgsInListPaths:        
+        imgLocation = None
+        possibsIdx = 1
+        # Go through the possibilities for this level
+        for imgPath in listImgPaths:
+            imgLocation = pyautogui.locateOnScreen(imgPath, region=regionRect)
+            # print(idx, possibsIdx, imgLocation, regionRect)
+            if imgLocation is None:
+                # Try next img in list for this level
+                continue
+            regionRect = (imgLocation.left, imgLocation.top, imgLocation.width, imgLocation.height)
+            possibsIdx += 1
+        # None of the possibilities were found at this level, abort
+        if imgLocation is None:
+            return idx-1    # return idx of last level that had an img found        
+    # Final img NOT found
+    if imgLocation is None:
+        return -1 # error!
+    # Final img has been found
+    center = pyautogui.center(imgLocation)
+    pyautogui.click(center.x, center.y)
+    return idx-1   # return idx of last img that was found, which is also the last img in this case
+  
+
 def findImgInListAndClick(listImgPaths):
     idx = 1
     for imgPath in listImgPaths:
